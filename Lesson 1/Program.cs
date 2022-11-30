774d6c797c7dd649942f39c3efad42d6b5684f42
Immunomodyluator <venhesterhack@gmail.com>
2022-11-30 12:22:44 +0300

    Урок 1 Задание 2

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
