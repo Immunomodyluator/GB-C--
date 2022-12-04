e0b785ff2f207a77f5ac77a2329a5e3ead8cc36d
Immunomodyluator <venhesterhack@gmail.com>
2022-12-04 14:52:51 +0300

    Урок 2 Задание 15

---
+++ Lesson 2/Задание 15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day >= 0 && day <= 5)
+{
+    Console.WriteLine("Нет");
+} else if(day >= 6 && day <= 7)
+{
+    Console.WriteLine("Да");
+} else
+{
+    Console.WriteLine("Введите число от 1 до 7");
+}
